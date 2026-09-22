ebe5ee87c0aaec1aa7ded9ed48a223688497444d
Jinken Nguyen <tientrieu19892025@users.noreply.github.com>
2026-09-22 18:56:11 +0700

    Remove VideoAdsSpeed from the public repo.
    Credit: Jinken Nguyen - 1989
    Donate: MB Bank 0345140889 Nguyen Tien Trieu

--- scripts/update-repo.py
+++ scripts/update-repo.py
@@ -213,7 +213,7 @@ FEATURED = [
     "com.jinkennguyen.noxframe",
     "com.jinkennguyen.settingsz",
     "com.jinkennguyen.appsw",
-    "com.jinkennguyen.videoadsspeed",
+]
 ]
 
 DEB_NAME_RE = re.compile(
